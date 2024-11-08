3c2cea437ce2793885d580cab71749dfa64fd795
Pedro Henrique <pdrocampos.santos@gmail.com>
2024-11-07 23:38:56 -0300

    First commit of 100-days-of-python

---
+++ Day_1/band_name_generator.py
@@ -0,0 +1,4 @@
+print("Welcome to the Band Name Generator.")
+city = input("Whats the name of the city you grew up in?\n")
+namePet = input("What's yout pet's name?\n")
+print("Your band name could be " + city + " " + namePet)
